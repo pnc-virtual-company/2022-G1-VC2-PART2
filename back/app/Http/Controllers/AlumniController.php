d2b6cc3a7e43fb40fa166f18bd7700ac90c64968
Vorn-Sreymao <sreymao.vorn@student.passerellesnumeriques.org>
2022-09-10 15:39:19 +0700

    #4 update information alumni

---
+++ back/app/Http/Controllers/AlumniController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alumni;
+use Illuminate\Http\Request;
+
+class AlumniController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Alumni  $alumni
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Alumni $alumni)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Alumni  $alumni
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $alumni =Alumni::findOrFail($id);
+        $alumni->batch = $request->batch;
+        $alumni->gender = $request->gender;
+        $alumni->phone = $request->phone;
+        $alumni->major = $request->major;
+        $alumni->save();
+        return response()->json(["message"=>"Alumni updated", 'data'=>$alumni],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Alumni  $alumni
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Alumni $alumni)
+    {
+        //
+    }
+}
