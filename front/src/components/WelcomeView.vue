2fd64aeb323918da1c58c1dddf2f48f93df31fc0
Tim-BinCoding <timsave98@gmail.com>
2022-09-10 15:18:37 +0700

    clearfile

---
+++ front/src/components/WelcomeView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>WELCOME TO VIRTUAL COMPANY II 2022</div>
+</template>
---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" width="100">
+    <Welcome/>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Welcome from '@/components/WelcomeView';
+export default {
+  name: 'HomeView',
+  components: {
+    Welcome
+  }
+}
+</script>
