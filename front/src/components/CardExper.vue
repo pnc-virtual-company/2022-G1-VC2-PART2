ea7c5a0b97056ce075bfadd7afce6d42221129e6
Tim-BinCoding <timsave98@gmail.com>
2022-09-12 15:32:14 +0700

    #14 Create form add and edit work experience alumni

--- front/src/components/CardExper.vue
+++ front/src/components/CardExper.vue
@@ -2,9 +2,9 @@
     <div class="bg-blue-200 p-3 rounded mt-4">
         <div class="flex justify-between">
             <h1 class="font-bold text-lg"><slot></slot></h1>
-            <div>
+            <div @click="$emit('formExper', 'Add')">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 shadow bg-white rounded-full">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
                 </svg>
             </div>
         </div>
@@ -12,14 +12,16 @@
             <edu-card v-for="ed of edu" :key="ed" :image="ed.src">
                 <h1 class="font-semibold">{{ed.school}}</h1>
                 <p class="text-sm leading-3">{{ed.degree}}, {{ed.major}}</p>
-                <p class="text-[13px] font-medium">{{ed.start_year}}-{{ed.end_year}}</p>
+                <p class="text-[13px] font-medium">{{ed.start_year}} / {{ed.end_year}}</p>
             </edu-card>
         </div>
         <div v-if="experiences">
-            <edu-card v-for="experience of experiences" :key="experience" :image="experience.src">
+            <edu-card v-for="(experience, i) in experiences" 
+            :key="experience" :image="experience.src" 
+            @click="$emit('cardEditor', {status:'Edit', index:i})">
                 <h1 class="font-semibold">{{experience.position}}</h1>
                 <p class="text-sm leading-3">{{experience.company_name}}</p>
-                <p class="text-[13px] font-medium">{{experience.start_year}}-{{experience.end_year}}</p>
+                <p class="text-[13px] font-medium">{{experience.start_year}} / {{experience.end_year}}</p>
             </edu-card>
         </div>
     </div>      
@@ -31,8 +33,7 @@ export default {
     components:{
       'edu-card': EduCard,
     },
-    props: ['edu', 'experiences']
-
+    props: ['edu', 'experiences'],
 };
 </script>
 
