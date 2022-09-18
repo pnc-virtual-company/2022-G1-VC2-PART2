308fb19c03d1f693c7c4635dd41f6bdca6b2c775
VANSAO <vansao.hang@student.passerellesnumeriques.org>
2022-09-18 09:57:04 +0700

    #41 Can search university

--- front/src/views/Alumni/FormInput/FormEditEduView.vue
+++ front/src/views/Alumni/FormInput/FormEditEduView.vue
@@ -2,15 +2,25 @@
 <div tabindex="-1" class=" bg-[#000000b9] fixed  flex items-center z-50 md:inset-0 h-modal md:h-full">
     <div class="modal bg-white h-auto shadow-md rounded mt-2 mb-10 m-auto w-[40%] z-10">
         <div class="w-full bg-skyblue p-2">
-            <h4 class="font-bold text-center text-white text-[20px]">Add Education</h4>
+            <h4 class="font-bold text-center text-white text-[20px]">Edit Education</h4>
         </div>
         <div class="p-5">
             <div class="w-[100%] my-2 p-2">
                 <label class="w-[12rem] text-start text-sm font-bold">University: </label>
-                <select v-model="university_id" class="mt-2 block p-2 w-full outline-none text-gray-900 bg-gray-50 rounded-sm border border-gray-400 sm:text-xs focus:border-[#22bbea]">
+                <!-- <select v-model="university_id" class="mt-2 block p-2 w-full outline-none text-gray-900 bg-gray-50 rounded-sm border border-gray-400 sm:text-xs focus:border-[#22bbea]">
                     <option selected disabled class="text-gray-900" value="null">Choose university</option>
                     <option v-for:="university in universities" :value='university.id'>{{university.name}}</option>
-                </select>
+                </select> -->
+                <input-edit-univer :universities="universities" :universityName="universityName" @university-name="getUniversityName" @university-id="getUniversitID"></input-edit-univer>
+            </div>
+            <div class="w-[100%] my-2 p-2">
+                    <label class="w-[12rem] text-start text-sm font-medium">Major </label>
+                    <input v-if="!isPNC && universityName !='Passerelles numériques Cambodia'" type="text" v-model="major" placeholder="name" class="mt-2 block p-2 w-full outline-none text-gray-900 bg-gray-50 rounded-sm border border-gray-400 sm:text-xs focus:ring-blue-500 focus:border-[#22bbea]">
+                    <select v-else v-model="major" class="mt-2 block p-2 w-full outline-none text-gray-900 bg-gray-50 rounded-sm border border-gray-400 sm:text-xs focus:ring-blue-500 focus:border-[#22bbea]">
+                        <option selected disabled class="text-gray-900" value="">Choose major</option>
+                        <option :value="'WEB'">WEB</option>
+                        <option :value="'SNA'">SNA</option>
+                    </select>
             </div>
             <div class="w-[100%] my-2 p-2">
                 <label class="w-[12rem] text-start text-sm font-bold">Degree: </label>
@@ -57,7 +67,7 @@
                     Cancel
                 </button>
                 <button @click="editEductaion()" class="bg-skyblue hover:bg-[#23afda] mx-2 text-white py-1 px-10 rounded focus:outline-none focus:shadow-outline">
-                    Add
+                    Save
                 </button>
             </div>
         </div>
@@ -65,7 +75,11 @@
 </div>
 </template>
 <script>
+import FormInputEditUniverVue from './FormInputEditUniver.vue';
 export default {
+    components:{
+        'input-edit-univer': FormInputEditUniverVue,
+    },
     emits: ['cancelEdit','EditEdu'],
   props: ["universities",'education'],
   data() {
@@ -86,12 +100,16 @@ export default {
       ],
       degrees: ["Associate", "Bachelor", "Master", "Doctorate"],
       university_id: this.education.university_id,
+      universityName: this.education.university.name,
       degree: this.education.degree,
+      major: this.education.major,
       start_month: this.education.start_month,
       start_year: this.education.start_year,
       end_month: this.education.end_month,
       end_year: this.education.end_year,
       edu_id: this.education.id,
+      isPNC:false,
+      PNC:"Passerelles numériques Cambodia"
     };
   },
   methods: {
@@ -103,10 +121,27 @@ export default {
           end_month: this.end_month,
           end_year: this.end_year,
           degree: this.degree,
+          major: this.major,
           university_id: this.university_id,
         };
         this.$emit("EditEdu", newEdu,this.edu_id);
-      } 
+      } ,
+      getUniversityName(universityName){
+          this.universityName = universityName;
+          if (universityName == this.PNC){
+            this.degrees = ['Associate']
+            this.degree = 'Associate';
+            this.isPNC = true;
+          }else{
+            this.degrees =["Associate", "Bachelor", "Master", "Doctorate"]
+            this.isPNC = false;
+          }
+        },
+        getUniversitID(university_id){
+            console.log(university_id);
+            this.university_id = university_id;
+        },
   },
+
 };
 </script>
