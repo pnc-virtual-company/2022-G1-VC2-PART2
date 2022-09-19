7f5ca70575146e543163f7b663d2b73134a0f099
Vorn-Sreymao <sreymao.vorn@student.passerellesnumeriques.org>
2022-09-19 07:59:22 +0700

    #42 input alumni information

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,5 +1,16 @@
 
 <template>
   <h1>home view</h1>
+  <FromInputInforAlumni></FromInputInforAlumni>
 </template>
+<script>
+  import FromInputInforAlumni from './Alumni/FormInput/FormInputInfoView.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      FromInputInforAlumni
+    }
+  }
+  </script>
 
