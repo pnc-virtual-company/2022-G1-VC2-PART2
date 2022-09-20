9311bc28aef54656c393c4197a1269fdc0b23b66
THEAVY.VUN <THEAVY.VUN@pnc.lan>
2022-09-20 19:24:58 +0700

    pull

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,20 +1,4 @@
 
 <template>
-<<<<<<< HEAD
-  <ManageAlumni/>
- 
-</template>
-
-<script>
-import ManageAlumni from "./Alumni/ManageAlumni/ManageAlumniView.vue"
-export default {
-  components:{
-    ManageAlumni,
-
-  }
-}
-</script>
-=======
   <h1>ALUMNI MANAGEMENT APPLICATION</h1>
 </template>
->>>>>>> 7b6f696c76db296f0df360f12568afce05740fbe
