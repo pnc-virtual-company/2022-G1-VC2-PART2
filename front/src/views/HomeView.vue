7b6f696c76db296f0df360f12568afce05740fbe
VANSAO <vansao.hang@student.passerellesnumeriques.org>
2022-09-20 11:15:00 +0700

    merge a conflict

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,16 +1,4 @@
 
 <template>
-  <Ero/>
+  <h1>ALUMNI MANAGEMENT APPLICATION</h1>
 </template>
-<<<<<<< HEAD
-
-<script>
-import Ero from "./Ero/EroView.vue"
-export default {
-  components:{
-    Ero,
-  }
-}
-</script>
-=======
->>>>>>> a6be3f33ce5c9e3bcd0f1b3d7b52a0b2b017dea6
