9fd74e269b21a576b072b8c55396023db3c493e1
THEAVY.VUN <THEAVY.VUN@pnc.lan>
2022-09-21 18:34:46 +0700

    change style

--- front/src/views/Ero/EroView.vue
+++ front/src/views/Ero/EroView.vue
@@ -8,7 +8,7 @@
                 <th class="text-center w-[30%]">ACTION</th>
             </tr>
         </thead>
-        <tbody class="w-full" >
+        <tbody v-if="companies.length > 0" class="w-full" >
             <tr v-for:="company in companies" class="border-b-[1px] border-gray-400 bg-gray-300">
                 <td class="text-center">
                     <div class="flex items-center space-x-4 p-2 ml-8">
@@ -25,6 +25,14 @@
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                         </svg>
                     </div>
+                </td>
+            </tr>
+        </tbody>
+        <tbody v-if="companies.length <= 0">
+            <tr class="bg-gray-300">
+                <td colspan="5" class="p-2 text-center">
+                    <img class="w-32 m-auto mt-3" src="./../../assets/notfound.png" alt="Image not found">
+                    <p class="mb-5">No companies found!</p>
                 </td>
             </tr>
         </tbody>
