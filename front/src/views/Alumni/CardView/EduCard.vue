2261fc25736d143ccba47b94979c1102ee349b2b
THEAVY.VUN <THEAVY.VUN@pnc.lan>
2022-09-22 16:35:01 +0700

    delete edu bug

--- front/src/views/Alumni/CardView/EduCard.vue
+++ front/src/views/Alumni/CardView/EduCard.vue
@@ -33,7 +33,7 @@
                             </span>
                             Edit
                         </p>
-                        <p class="flex items-center hover:cursor-pointer hover:bg-red-500 py-1 px-3 hover:text-white hover:rounded-tr hover:rounded-tl">
+                        <p class="flex items-center hover:cursor-pointer hover:bg-red-500 py-1 px-3 hover:text-white hover:rounded-tr hover:rounded-tl" @click="$emit('removeEdu', education.id), isClick=false" > 
                             <span class="mr-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -73,9 +73,6 @@ export default {
         onClickOutSide() {
             this.isClick = !this.isClick;
         },
-        removeEdu() {
-            this.isClick = !this.isClick;
-        }
     },
     computed: {
         educations() {
