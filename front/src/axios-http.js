a417e107b3ef8180784a0a09f9876a5a85a314b6
THEAVY.VUN <THEAVY.VUN@pnc.lan>
2022-09-27 14:56:39 +0700

    change style on form register ero

--- front/src/axios-http.js
+++ front/src/axios-http.js
@@ -1,14 +1,3 @@
-// import axios from "axios";
-// let axiosAPI=axios.create({
-//   baseURL: 'http://127.0.0.1:8000/api/',
-//   headers: {
-//     "Content-type": "application/json",
-//     // 'Authorization': 'Bearer ' +  JSON.parse(localStorage.getItem('token'))
-//     'Authorization': 'Bearer ' +  "25|x5ZTJPWaEkNQPFaexWotr2To3JilnOeCUkPy9Xc4"
-//   }
-// });
-// export default axiosAPI;
-
 import axios from "axios";
 import getCookie from "./helper/getCookie";
 import decryptData from "./helper/dencryptData";
--- front/src/views/Authentication/RegisterEroView.vue
+++ front/src/views/Authentication/RegisterEroView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="p-4 bg-[#23242986] fixed h-full w-full flex items-center top-0 z-100 m-auto">
-      <div class="rounded mb-2 w-[40%] m-auto mt-[15px] bg-[#CCE7F6]" v-click-outside="onClickOutSide">
-        <div class="cancel flex justify-end w-[100%] m-auto p-2">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 hover:text-red-500 cursor-pointer" viewBox="0 0 20 20" fill="currentColor" @click="$emit('popUp', false)">
+      <div class="rounded mb-2 w-[40%] m-auto mt-[15px] bg-white" v-click-outside="onClickOutSide">
+        <form class="p-3 rounded border-white" @submit.prevent="register">
+          <div class="flex justify-between items-center">
+            <p></p>
+            <h1 class="text-xl font-semibold text-center">
+              ERO RIGISTER
+            </h1>
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 hover:text-red-500 cursor-pointer" viewBox="0 0 20 20" fill="currentColor" @click="$emit('popUp', false)">
               <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
             </svg>
-        </div>
-        <form class="p-3 rounded border-white" @submit.prevent="register">
-          <h1 class="text-2xl font-semibold text-center">
-            ERO RIGISTER
-          </h1>
+
+          </div>
           <div class="text-start">
               <div class="w-[100%] flex mt-5 justify-between ">
                   <div class="w-[49%] m-1">
@@ -18,7 +20,7 @@
                     </label>
                     <input
                       v-model="first_name"
-                      class="shadow border rounded w-full py-2.5 px-2 text-sm text-gray-700 mb-1 leading-tight outline-none focus:shadow-outline focus:border-[#22bbea] reguired"
+                      class="border-[1px] border-gray-300 rounded w-full py-2.5 px-2 text-sm text-gray-700 mb-1 leading-tight outline-none focus:shadow-outline focus:border-[#22bbea] reguired"
                       :class="{ 'border-red-500 bg-red-100': is_firstname}"
                       type="text"
                       placeholder="First name..."
@@ -30,23 +32,23 @@
                     </label>
                     <input
                       v-model="last_name"
-                      class="shadow border rounded w-full py-2.5 px-2 text-sm text-gray-700 mb-1 leading-tight outline-none focus:shadow-outline focus:border-[#22bbea]"
+                      class="border-[1px] border-gray-300 rounded w-full py-2.5 px-2 text-sm text-gray-700 mb-1 leading-tight outline-none focus:shadow-outline focus:border-[#22bbea]"
                       :class="{ 'border-red-500 bg-red-100': is_lastname}"
                       type="text"
                       placeholder="Last name..."
                     />
                   </div>
               </div>
-              <div class="mb-2 relative w-[99%] m-atuo">
-                <label class="block text-gray-700 text-lg mb-1 ml-1" for="email">
+              <div class="mb-2 relative w-[98%] m-atuo m-1">
+                <label class="block text-gray-700 text-lg mb-1" for="email">
                   Email <span class="text-red-500">*</span>
                 </label>
-                <input v-model="email" class="shadow border rounded w-full py-2.5 px-2 text-sm text-gray-700 mb-1 leading-tight outline-none focus:shadow-outline focus:border-[#22bbea]" :class="{ 'border-red-500 bg-red-100': is_email}" id="email" type="email" placeholder="email..."
+                <input v-model="email" class="border-[1px] border-gray-300 rounded w-full py-2.5 px-2 text-sm text-gray-700 mb-1 leading-tight outline-none focus:shadow-outline focus:border-[#22bbea]" :class="{ 'border-red-500 bg-red-100': is_email}" id="email" type="email" placeholder="email..."
                 />
               </div>
               <p class="text-medium">{{inviteMessage}}</p>
               <button class="bg-[#22BBEA] text-white py-2 w-[98%] px-4 rounded focus:shadow-outline ml-1 mb-2 mt-4">
-              Create
+              Invite
               </button> 
           </div>
         </form>
