56088da9564d5fb59f00509ff4acc20757275cce
VANSAO <vansao.hang@student.passerellesnumeriques.org>
2022-09-28 17:15:30 +0700

    Fix bug

--- front/src/views/Alumni/AllAlumniView.vue
+++ front/src/views/Alumni/AllAlumniView.vue
@@ -1,21 +1,4 @@
 <template>
-    <!-- <div  class="flex justify-between mt-2 p-2 border-b-[1px] border-[#a9aaaaa3] w-[50%] m-auto" >
-        <div class="flex items-center">
-            <div class="w-16 h-16">
-                <img class="w-16 h-16 rounded-full border-[1px] border-gray-400 object-cover" src="../../assets/cutes.jpg" alt="">
-            </div>
-            <div class="ml-4">
-                <h1 class="text-sm leading-3 font-bold">Vansao Hang</h1>
-                <p class="font-medium">PN Cambodia</p>
-                <p class="text-[13px] ">Camsolution</p>
-            </div>
-        </div>
-        <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:cursor-pointer hover:font-lg">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-            </svg>
-        </div>
-    </div> -->
     <alumni-explore></alumni-explore>
 </template>
 
